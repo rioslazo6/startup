289186749391d8d5e2e5f9d65cc0852d9aa85ba9
German Rios-Lazo <germanrl@byu.edu>
2024-02-27 23:55:18 -0700

    Added outer glow when clicking pokemon card

--- select.js
+++ select.js
@@ -1,5 +1,13 @@
 // This will run only after the DOM is fully loaded, to avoid the span element being null.
 document.addEventListener("DOMContentLoaded", function(event) {
-    let usernameSpan = document.getElementById("usernameSpan")
+    const usernameSpan = document.getElementById("usernameSpan")
     usernameSpan.textContent = localStorage.getItem("username")
+    pokemonCards = document.querySelectorAll("div.pokemon-card")
+    pokemonCards.forEach(card => {
+        card.addEventListener("click", (e) => {
+            card.classList.add("selected")
+        })
+    })
 })
+
+
