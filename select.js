ffaeb8726a44d429288abf0cf14ba4cda7e1f256
German Rios-Lazo <germanrl@byu.edu>
2024-02-28 00:01:47 -0700

    Limiting selection to 1 pokemon card

--- select.js
+++ select.js
@@ -1,3 +1,5 @@
+let pokemonCards = undefined
+
 // This will run only after the DOM is fully loaded, to avoid the span element being null.
 document.addEventListener("DOMContentLoaded", function(event) {
     const usernameSpan = document.getElementById("usernameSpan")
@@ -5,9 +7,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
     pokemonCards = document.querySelectorAll("div.pokemon-card")
     pokemonCards.forEach(card => {
         card.addEventListener("click", (e) => {
-            card.classList.add("selected")
+            refreshCardSelection(card.id)
         })
     })
 })
+
+function refreshCardSelection(id) {
+    pokemonCards.forEach(card => {
+        if (card.id === id) {
+            card.classList.add("selected")
+        } else {
+            card.classList.remove("selected")
+        }
+    })
+}
 
 
