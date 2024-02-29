dccced28727c53d1465165495331b296f3534182
German Rios-Lazo <germanrl@byu.edu>
2024-02-28 23:08:18 -0700

    Simulating back and forth moves in the battle page

--- battle.js
+++ battle.js
@@ -4,4 +4,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
     myPokemonImage = document.getElementById("myPokemon")
     myPokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/${selectedPokemonId}.gif`
     document.getElementById("myPokemonName").innerHTML = localStorage.getItem("selectedPokemonName")
+
+    moves = document.querySelectorAll("td.move")
+    moves.forEach(move => {
+        move.addEventListener("click", (e) => {
+            const pokemonSpan = document.getElementById("pokemonSpan")
+            pokemonSpan.innerHTML = localStorage.getItem("selectedPokemonName")
+            const moveName = move.innerHTML
+            const moveSpan = document.getElementById("moveSpan")
+            moveSpan.innerHTML = moveName
+            // 2 seconds after the user clicks a move, the opponent will "attack".
+            setTimeout(() => {
+                pokemonSpan.innerHTML = "Squirtle"
+                moveSpan.innerHTML = "WATER GUN"
+            }, 2000)
+        })
+    })
 })
