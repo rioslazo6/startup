ccbd4c80665b2a188a70c8e4fcf7db37fc0b42d7
German Rios-Lazo <germanrl@byu.edu>
2024-02-28 23:37:54 -0700

    Simulating registering new user

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 function login() {
+    const newUser = document.getElementById("registerCheckbox").checked
     const usernameInput = document.querySelector("#username")
     const passwordInput = document.querySelector("#password")
     const correctPassword = credentials[usernameInput.value]
-    if (passwordInput.value === correctPassword) {
+    if (newUser) {
+        if (usernameInput.value.trim().length === 0 || passwordInput.value.trim().length === 0) {
+            window.alert("Please enter a valid username and password to register.")
+        } else {
+            credentials[usernameInput.value] = passwordInput.value // "Adding" new user to "database".
+            localStorage.setItem("username", usernameInput.value)
+            console.log(credentials)
+            window.location.href = "select.html"
+        }
+    } else if (passwordInput.value === correctPassword) {
         localStorage.setItem("username", usernameInput.value)
         window.location.href = "select.html"
     } else {
