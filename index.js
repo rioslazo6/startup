4fb5bf9ab3333eeefababc8d6a949dc9842972c2
German Rios-Lazo <germanrl@byu.edu>
2024-03-11 17:43:57 -0600

    Setup service to get leaderboard

--- index.js
+++ index.js
@@ -10,6 +10,56 @@ app.use(express.json())
 // Serve up the front-end static content hosting.
 app.use(express.static("public"))
 
+// Router for service endpoints.
+var apiRouter = express.Router()
+app.use("/api", apiRouter)
+
+// Get leaderboard.
+apiRouter.get("/leaderboard", (_req, res) => {
+    res.send(leaderboardData)
+})
+
+// Return application's default page if path is unknown.
+app.use((_req, res) => {
+    res.sendFile("index.html", { root: "public" })
+  });
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
+
+// Temporary mock data.
+let leaderboardData = [
+    {
+        username: "ash",
+        totalBattles: 100,
+        battlesWon: 88,
+        get winPercentage() {
+            return parseFloat((this.battlesWon / this.totalBattles) * 100).toFixed(2) + " %"
+        }
+    },
+    {
+        username: "silver",
+        totalBattles: 250,
+        battlesWon: 200,
+        get winPercentage() {
+            return parseFloat((this.battlesWon / this.totalBattles) * 100).toFixed(2) + " %"
+        }
+    },
+    {
+        username: "zelda",
+        totalBattles: 15,
+        battlesWon: 10,
+        get winPercentage() {
+            return parseFloat((this.battlesWon / this.totalBattles) * 100).toFixed(2) + " %"
+        }
+    },
+    {
+        username: "link",
+        totalBattles: 2,
+        battlesWon: 1,
+        get winPercentage() {
+            return parseFloat((this.battlesWon / this.totalBattles) * 100).toFixed(2) + " %"
+        }
+    }
+]
--- public/leaderboard.js
+++ public/leaderboard.js
@@ -1,77 +1,47 @@
-// This is a "database" of the leaderboards.
-const leaderboardData = [
-    {
-        username: "ash",
-        totalBattles: 100,
-        battlesWon: 88,
-        get winPercentage() {
-            return parseFloat((this.battlesWon / this.totalBattles) * 100).toFixed(2) + " %"
-        }
-    },
-    {
-        username: "silver",
-        totalBattles: 250,
-        battlesWon: 200,
-        get winPercentage() {
-            return parseFloat((this.battlesWon / this.totalBattles) * 100).toFixed(2) + " %"
-        }
-    },
-    {
-        username: "zelda",
-        totalBattles: 15,
-        battlesWon: 10,
-        get winPercentage() {
-            return parseFloat((this.battlesWon / this.totalBattles) * 100).toFixed(2) + " %"
-        }
-    },
-    {
-        username: "link",
-        totalBattles: 2,
-        battlesWon: 1,
-        get winPercentage() {
-            return parseFloat((this.battlesWon / this.totalBattles) * 100).toFixed(2) + " %"
+async function loadLeaderboard() {
+    let leaderboardData = []
+    try {
+        // Getting latest leaderboard from the service.
+        const response = await fetch("api/leaderboard")
+        leaderboardData = await response.json()
+
+        // Saving locally.
+        localStorage.setItem("leaderboard", JSON.stringify(leaderboardData))
+    } catch {
+        // Using local copy if there's an error.
+        const leaderboardString = localStorage.getItem("leaderboard")
+        if (leaderboardString) {
+            leaderboardData = JSON.parse(leaderboardString)
         }
     }
-]
+    addDataToTable(leaderboardData)
+}
 
 function addDataToTable(data) {
     let headers = []
     for (const [key, value] of Object.entries(data[0])) {
-        headers.push({ name: key, type: typeof value });
+        headers.push({ name: key, type: typeof value })
     }
-    const table = document.getElementById("leaderboard").getElementsByTagName('tbody')[0]
+    const table = document.getElementById("leaderboard").getElementsByTagName("tbody")[0]
     table.innerHTML = ""
-    const row = document.createElement("tr");
+    const row = document.createElement("tr")
     table.appendChild(row)
     headers.forEach((header) => {
-        const cell = document.createElement("th");
-        row.appendChild(cell);
-        const textNode = document.createTextNode(header.name);
-        cell.appendChild(textNode);
+        const cell = document.createElement("th")
+        row.appendChild(cell)
+        const textNode = document.createTextNode(header.name)
+        cell.appendChild(textNode)
     })
     data.forEach((dataRow) => {
-        const row = document.createElement("tr");
-        table.appendChild(row);
+        const row = document.createElement("tr")
+        table.appendChild(row)
         for (const [, value] of Object.entries(dataRow)) {
-            const cell = document.createElement("td");
-            row.appendChild(cell);
-            const textNode = document.createTextNode(value);
-            cell.appendChild(textNode);
+            const cell = document.createElement("td")
+            row.appendChild(cell)
+            const textNode = document.createTextNode(value)
+            cell.appendChild(textNode)
         }
-    });
+    })
 }
 
-// This will run only after the DOM is fully loaded, to make sure the table exists.
-document.addEventListener("DOMContentLoaded", function(event) {
-    addDataToTable(leaderboardData)
-})
-
-// Every 2 seconds, a random user will either "win" or "lose" a battle, simulating multiple players using the app.
-setInterval(() => {
-    const index = Math.floor(Math.random() * 4)
-    leaderboardData[index].totalBattles++
-    if (Math.random() >= 0.5) {
-        leaderboardData[index].battlesWon++
-    }
-    addDataToTable(leaderboardData)
-}, 2000);
+loadLeaderboard()
