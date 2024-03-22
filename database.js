50d622b028fd8a26d699d4b5d667b2b5da41fa45
German Rios-Lazo <germanrl@byu.edu>
2024-03-22 11:26:07 -0600

    Added functions to create and retrieve users from db

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
-const { MongoClient } = require('mongodb')
-const config = require('./dbConfig.json')
-const bcrypt = require('bcrypt')
-const uuid = require('uuid')
+const { MongoClient } = require("mongodb")
+const config = require("./dbConfig.json")
+const bcrypt = require("bcrypt")
+const uuid = require("uuid")
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`
 const client = new MongoClient(url)
-const db = client.db('startup')
+const db = client.db("startup")
+const userCollection = db.collection("user")
 
 // Testing database connection
 (async function testConnection() {
@@ -15,3 +16,27 @@ const db = client.db('startup')
     console.log(`There was an error connecting to the database: ${ex.message}`)
     process.exit(1)
 })
+
+function getUser(username) {
+    return userCollection.findOne({ username: username})
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token })
+}
+
+async function createUser(username, password) {
+    const user = {
+        username: username,
+        password: await bcrypt.hash(password, 10), // Hashing password
+        token: uuid.v4()
+    }
+    await userCollection.insertOne(user)
+    return user
+}
+
+module.exports = {
+    getUser,
+    getUserByToken,
+    createUser
+}
